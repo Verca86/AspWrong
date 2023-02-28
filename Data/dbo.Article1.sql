12f96fd427e640699aa011b12572336643c95985
Verca86 <122152673+Verca86@users.noreply.github.com>
2023-02-28 16:25:27 +0100

    Add files via upload

---
+++ Data/dbo.Article1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Article1] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [Jméno]           NVARCHAR (MAX) NOT NULL,
+    [Příjmení]        NVARCHAR (MAX) NOT NULL,
+    [Adresa]          NVARCHAR (MAX) NOT NULL,
+    [Obec]            NVARCHAR (MAX) NOT NULL,
+    [PSČ]             INT            NOT NULL,
+    [Telefon] NVARCHAR (MAX) NOT NULL,
+    [Email] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Article1] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
